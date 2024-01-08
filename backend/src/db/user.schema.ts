8867f82c47082972b998b0639281fd744c879035
csubiii <csubicsabi05@gmail.com>
2024-01-08 22:23:20 +0100

    feat: schema for the 'User' collection

---
+++ backend/src/db/user.schema.ts
@@ -0,0 +1,13 @@
+import mongoose, { Schema } from 'mongoose';
+import { UserDocument } from '../types/types';
+
+const userSchema: Schema<UserDocument> = new Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  profilePicture: { type: String },
+  bio: { type: String },
+  friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+});
+
+export default mongoose.model<UserDocument>('User', userSchema);
