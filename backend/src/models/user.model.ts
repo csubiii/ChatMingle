b8a15cc7617ac8eebf3440043eb41e626ea675ee
csubiii <csubicsabi05@gmail.com>
2024-03-20 21:49:26 +0100

    chore: import rename

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,5 +1,5 @@
 import { model } from 'mongoose';
-import { userSchema } from '../db/user.schema';
 import { UserDocument } from '../types/types';
+import userSchema from '../db/userSchema.schema';
 
 export const User = model<UserDocument>('User', userSchema);
