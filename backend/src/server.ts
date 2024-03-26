b992694bb57bd70ebdebda93edf55ccfed4b0d69
csubiii <csubicsabi05@gmail.com>
2024-03-26 22:09:35 +0100

    test: websocket connection testing

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { Request, Response } from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 import { connectToDatabase } from './db/connection';
 import registrationRouter from './routes/registration.route';
 import userAuthenticateRouter from './routes/userAuthenticate.route';
 import protectedRouter from './routes/protected.route';
-const punycode = require('punycode/');
 
 const app = express();
 app.use(express.json());
@@ -13,15 +13,20 @@ dotenv.config();
 connectToDatabase();
 
 const PORT_STRING = process.env.PORT || '3000';
-
-// Parse PORT as a number
 const PORT_NUMBER = parseInt(PORT_STRING, 10);
 
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
 app.use(registrationRouter);
-app.use(userAuthenticateRouter)
+app.use(userAuthenticateRouter);
 app.use(protectedRouter);
 
-
-app.listen(PORT_NUMBER, () => {
+httpServer.listen(PORT_NUMBER, () => {
    console.log("Server Listening on PORT:", process.env.PORT || '3000');
 });
+
+io.on("connection", (socket) => {
+   console.log("A user connected");
+   // Handle socket events here
+ });
