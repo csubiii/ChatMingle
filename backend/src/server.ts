7c5c0caa29466b5c0025b471b9c48cd12da22cf4
csubiii <csubicsabi05@gmail.com>
2024-03-31 23:43:38 +0200

    test: Socket.io connection and message handling

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,27 +6,44 @@ import { connectToDatabase } from './db/connection';
 import registrationRouter from './routes/registration.route';
 import userAuthenticateRouter from './routes/userAuthenticate.route';
 import protectedRouter from './routes/protected.route';
+import cors from 'cors';
 
-const app = express();
-app.use(express.json());
 dotenv.config();
-connectToDatabase();
 
 const PORT_STRING = process.env.PORT || '3000';
 const PORT_NUMBER = parseInt(PORT_STRING, 10);
 
+const app = express();
+connectToDatabase();
+
 const httpServer = createServer(app);
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+   cors: {
+      origin: 'http://localhost:5173',
+   }
+});
+
+app.use(cors())
+app.use(express.json());
 
 app.use(registrationRouter);
 app.use(userAuthenticateRouter);
 app.use(protectedRouter);
 
+io.on("connection", (socket) => {
+   console.log("A user connected");
+ 
+   socket.on("disconnect", () => {
+     console.log("User disconnected");
+   });
+ 
+   socket.on("chat message", (msg) => {
+     console.log("Message received: ", msg);
+     io.emit("chat message", msg);
+   });
+ });
+
+
 httpServer.listen(PORT_NUMBER, () => {
    console.log("Server Listening on PORT:", process.env.PORT || '3000');
 });
-
-io.on("connection", (socket) => {
-   console.log("A user connected");
-   // Handle socket events here
- });
--- frontend/ChatMingle/src/App.tsx
+++ frontend/ChatMingle/src/App.tsx
@@ -1,12 +1,47 @@
-import React from 'react'
-import { Button } from './components/ui/button'
+import React, { useEffect, useState } from 'react';
+import io, { Socket } from 'socket.io-client';
 
-const App = () => {
+const App: React.FC = () => {
+  const [message, setMessage] = useState<string>('');
+  const [socket, setSocket] = useState<Socket | null>(null); // Declare socket state
+
+  useEffect(() => {
+    const newSocket = io('ws://localhost:3002');
+
+    newSocket.on('connect', () => {
+      console.log('Connected to server');
+    });
+
+    newSocket.on('disconnect', () => {
+      console.log('Disconnected from server');
+    });
+
+    setSocket(newSocket);
+
+    return () => {
+      newSocket.disconnect();
+    };
+  }, []);
+
+  const sendMessage = () => {
+    if (socket) {
+      socket.emit('chat message', message);
+      setMessage('');
+    } else {
+      console.error('Socket is not connected');
+    }
+  };
+
   return (
     <div>
-      <Button>Click me</Button>
+      <input
+        type="text"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button onClick={sendMessage}>Send Message</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
